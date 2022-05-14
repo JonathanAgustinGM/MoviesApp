d7b8f4c4517b9e762b3ca482e62f08fd3911aea0
Jordy Gracia <graciamancilla@icloud.com>
2022-05-14 06:21:12 -0500

    sing in added

--- MoviesApp/Movies/View/Home.swift
+++ MoviesApp/Movies/View/Home.swift
@@ -6,28 +6,53 @@
 //
 
 import UIKit
-
-class Home: UIViewController {
+import FirebaseAuth
+class Home: UIViewController{
    
 
+    @IBOutlet weak var EmailTextfield: UITextField!
     @IBOutlet weak var Singin: UIButton!
+    @IBOutlet weak var registerboton: UIButton!
+    @IBOutlet weak var paswordtextfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        EmailTextfield.delegate = self
+        paswordtextfield.delegate = self
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func RegistrarBoton(_ sender: Any) {
+        if let useremail = EmailTextfield.text, let userpassword = paswordtextfield.text {
+        HomeViewModel.shared.SingUp(email: useremail, password: userpassword)
+    }
+    }
     @IBAction func SingConfirm(_ sender: Any) {
-        MoviesExternalData.shared.getTrendingList(type: "all") { [self] trending in
-            DispatchQueue.main.async {
-                
-                self.performSegue(withIdentifier: "movieshomesegue", sender: self)
-            }
-        } failure: { error in
-            print(error!)
+        if let email = EmailTextfield.text, let password = paswordtextfield.text{
+       IniciarSesion(email: email, password: password)
+        
+        
         }
-        
-        
-    
+}
+    func IniciarSesion(email:String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let result = result, error == nil {
+                MoviesExternalData.shared.getTrendingList(type: "all") { trending in
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "movieshomesegue", sender: self)
+                    }
+                } failure: { error in
+                    print("n oData")
+                }
+
+                
+        }
+
+}
+    }
 }
 
+extension Home: UITextFieldDelegate {
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder()
+}
 }
--- MoviesApp/Movies/ViewModel/HomeViewModel.swift
+++ MoviesApp/Movies/ViewModel/HomeViewModel.swift
@@ -7,9 +7,21 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
 
 class HomeViewModel {
-    func SingIn() {
+    let view = Home()
+    static let shared = HomeViewModel()
+    func SingIn(email: String, password: String) {
    
     }
+    func SingUp(email: String, password: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let result = result, error == nil {
+                DispatchQueue.main.async {
+                self.view.performSegue(withIdentifier: "movieshomesegue", sender: self)
+                }
+        }
+                                }
+     
 }
